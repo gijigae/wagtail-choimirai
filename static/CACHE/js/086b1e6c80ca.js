592579c70810bcb38ba89d7039d9211412344ddf
Sangmin Ahn <gijigae@gmail.com>
2015-12-03 20:19:38 +0900

    Include media and static folders

---
+++ static/CACHE/js/086b1e6c80ca.js
@@ -0,0 +1,1 @@
+var blog_page=window.location.pathname;$(function(){$(document).on('click','.next',function(e){e.preventDefault();$.scrollTo($('section.blog'),1000);$('#listing').load(blog_page,next_params,function(){});});$(document).on('click','.previous',function(e){e.preventDefault();$.scrollTo($('section.blog'),1000);$('#listing').load(blog_page,prev_params,function(){});});});
--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -100,7 +100,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '0qkkb_#_b)3g-h#*e^-a653*wo0=i2fjeebhh193wt(xe$=s3f'
+#SECRET_KEY = '0qkkb_#_b)3g-h#*e^-a653*wo0=i2fjeebhh193wt(xe$=s3f'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
