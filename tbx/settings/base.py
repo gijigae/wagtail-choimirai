e18c2cd0ba2e4dc024dd36ff60b05534fe341b44
Sangmin Ahn <gijigae@gmail.com>
2015-12-04 13:36:44 +0900

    Update S3 key setting

--- tbx/settings/base.py
+++ tbx/settings/base.py
@@ -244,7 +244,7 @@ env = os.environ.copy()
 # MEDIA_URL = 'http://%s.s3-website-ap-northeast-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
-AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
